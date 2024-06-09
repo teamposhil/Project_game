cb0a5f7ab3e9c9ae78f82b173d66c10438c8803e
DEV ! LOG <mhy9203@gmail.com>
2024-06-09 08:29:48 +0000

    DATA COMMIT

---
+++ project_hack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char passWord[20], command[20], code[7], file[7];
+    int lock;
+
+    printf("project: h@(k\n\n\n");
+    printf("-챕터 1-\n");
+    printf("오늘은 꿈만 같은 휴일이다.\n");
+    printf("일반 회사 둔갑한 우리 회사는 사실 북에 있는 스파이와 통신하는 국영조직이다.\n");
+    printf("*사장님*\n");
+    printf("뭐지? 오늘은 휴일인데, 전화가 왔다.\n");
+    printf("\'여보세..\'\n");
+    printf("\'김과장! 당장 회사로 오기 바라네!\'\n");
+    printf("\'네..? 하지만 오늘은 토요일이잖..\'\n");
+    printf("\'자세한 내용은 회사에서 하겠네!\'\n");
+    printf("*통화 종료음*\n");
+    printf("뭐지..? 혹시 모르니 회사에 가보자.\n\n");
+    printf("-챕터 2-\n");
+    printf("저기 사장님이 보인다. 말을 걸어보자.\n");
+    printf("\'사장님.\'\n");
+    printf("\'아, 수빈양! 알다시피 우리 회사는 사실 북 쪽에 있는 스파이와 통신하는 국영조직이라네. ");
+    printf("그런데, ")
+}
